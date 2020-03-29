847842e260bd3ce0a67199ccfda4a910423ccd0f
Vikas Gupta <60785282+h20190541@users.noreply.github.com>
2020-03-29 20:53:02 +0530

    Delete .demo.ko.cmd

--- Assignment2/.demo.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/vikas/eeeg547/G547/Assignment2/demo.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/vikas/eeeg547/G547/Assignment2/demo.ko /home/vikas/eeeg547/G547/Assignment2/demo.o /home/vikas/eeeg547/G547/Assignment2/demo.mod.o ;  true
