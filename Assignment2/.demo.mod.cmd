38efa21cad28c20ac9c72144abbc0bad620f6b25
Vikas Gupta <ec.vikas23@gmail.com>
2020-03-29 20:51:00 +0530

    Second Assignment

---
+++ Assignment2/.demo.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/vikas/eeeg547/G547/Assignment2/demo.mod := { echo  /home/vikas/eeeg547/G547/Assignment2/demo.o; echo; } > /home/vikas/eeeg547/G547/Assignment2/demo.mod
---
+++ Assignment2/usb.mod
@@ -0,0 +1,2 @@
+/home/vikas/eeeg547/G547/Assignment2/usb.o
+
